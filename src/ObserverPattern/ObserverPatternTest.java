645b0fd15d0d6a95ad2f44b9b6daad2a4890e7be
Mikołaj Świątek <mikolajswiatek@outlook.com>
2016-08-22 00:00:59 +0200

    Added tests for Observer pattern

---
+++ src/ObserverPattern/ObserverPatternTest.java
@@ -0,0 +1,40 @@
+package ObserverPattern;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ObserverPatternTest {
+    private ProductSubject subject;
+
+    @Before
+    public void setUp() {
+        subject = new ProductSubject("A1", "T!", false);
+    }
+
+    @Test
+    public void WhenAddedClientToProductShouldReturnCorrectLength() {
+        ClientObserver client1 = new ClientObserver("C1", null);
+        ClientObserver client2 = new ClientObserver("C2", null);
+
+        subject.register(client1);
+        subject.register(client2);
+
+        assertTrue(subject.getObservers().size() == 2);
+    }
+
+    @Test
+    public void WhenRemovedClientFromProductShouldReturnCorrectLength() {
+        ClientObserver client1 = new ClientObserver("C1", null);
+        ClientObserver client2 = new ClientObserver("C2", null);
+
+        subject.register(client1);
+        subject.register(client2);
+
+        subject.unregister(client1);
+        subject.unregister(client2);
+
+        assertTrue(subject.getObservers().size() == 0);
+    }
+}
